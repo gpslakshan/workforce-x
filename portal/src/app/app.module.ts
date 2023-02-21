e5a6b869c5df6b5d9d38d580c36067dcf2f8223f
Sachin <sachinlakshan04@gmail.com>
2023-02-22 00:05:27 +0530

    basic components created

--- portal/src/app/app.module.ts
+++ portal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { DepartmentsComponent } from './pages/departments/departments.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EmployeesComponent,
+    DepartmentsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
