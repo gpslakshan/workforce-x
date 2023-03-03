b799161c85b3c8953c5c6cde7c076dcecafca1cb
Sachin <sachinlakshan04@gmail.com>
2023-03-03 22:13:01 +0530

    employees module, controller, services and DTO created.

---
+++ api/dist/employees/dto/create-employee.dto.d.ts
@@ -0,0 +1,15 @@
+export declare class CreateEmployeeDto {
+    first_name: string;
+    last_name: string;
+    date_of_birth: string;
+    house_no: string;
+    street: string;
+    city: string;
+    state: string;
+    postcode: string;
+    email: string;
+    mobile: string;
+    salary: number;
+    department_id: number;
+    position_id: number;
+}
---
+++ api/src/employees/dto/create-employee.dto.ts
@@ -0,0 +1,16 @@
+export class CreateEmployeeDto {
+    first_name: string;
+    last_name: string;
+    date_of_birth: string;
+    house_no: string;
+    street: string;
+    city: string;
+    state: string;
+    postcode: string;
+    email: string;
+    mobile: string;
+    salary: number;
+    department_id: number;
+    position_id: number;
+}
+
