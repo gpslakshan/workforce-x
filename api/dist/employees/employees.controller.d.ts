6b30d6f444a111951b0971bdb9a49ccd51fc2726
Sachin <sachinlakshan04@gmail.com>
2023-03-04 13:36:00 +0530

    remove dist folder tracking

--- api/dist/employees/employees.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { EmployeesService } from './employees.service';
-export declare class EmployeesController {
-    private employeesService;
-    constructor(employeesService: EmployeesService);
-    getAllEmployees(): Promise<any>;
-    getEmployee(id: string): any;
-}
--- api/dist/employees/employees.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Employee } from './employee.model';
-export declare class EmployeesService {
-    private employeeModel;
-    constructor(employeeModel: typeof Employee);
-    findAll(): Promise<Employee[]>;
-}
